5463593b40c5144f45190376da40bf64abbf6c27
patilniraj7722 <patilniraj7722@gmail.com>
2022-12-05 15:41:06 +0530

    repo by niraj

---
+++ Autocare/src/main/java/com/Technosignia/Autocare/dao/JobCardRepository.java
@@ -0,0 +1,10 @@
+package com.Technosignia.Autocare.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Technosignia.Autocare.entity.JobCard;
+
+
+public interface JobCardRepository extends JpaRepository<JobCard,Long>{
+
+}
